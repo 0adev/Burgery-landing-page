b91df933b6376bb6da0a870312fe22425708181a
0adev <ayoubaguirar44@gmail.com>
2024-09-23 16:29:55 +0100

    Added new features and updates to the website

--- src/app.js
+++ src/app.js
@@ -49,8 +49,11 @@ const swiper = new Swiper(".swiper", {
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const menuList = document.querySelector(".nav-menu");
 const backToTopButton = document.querySelector(".back-to-top-btn");
+// menu page
 const categoryItems = document.querySelectorAll(".category .item");
 const menuCards = document.querySelectorAll(".menu-cards .card");
+// contact page > faqs
+const questions = document.querySelectorAll(".question");
 
 const currentPage = window.location.pathname;
 
@@ -119,7 +122,7 @@ function backToTop(e) {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
-// == filter burgers by category ==
+// == MENU PAGE > filter burgers by category ==
 const message = document.createElement("p");
 message.classList.add("ff-primary", "fs-xl", "fw-semibold", "text-primary");
 message.innerHTML = `
@@ -167,10 +170,43 @@ categoryItems.forEach((item) => {
     this.classList.add("active");
 
     filterCards(selectedCategory);
+    console.log(item);
   });
+});
+
+// == CONTACT PAGE > FAQS Toggle the questions ==
+
+questions.forEach((question) => {
+  function showTheAnswer() {
+    const plusIcon = question.querySelector("i"); // Icon specific to the clicked question
+    const answer = question.querySelector(".answer"); // Answer specific to the clicked question
+
+    // Toggle the visibility of the answer
+    if (answer.classList.contains("d-block")) {
+      answer.classList.remove("d-block");
+      answer.classList.add("d-none");
+      plusIcon.classList.remove("rotate-135deg");
+    } else {
+      // Close all other answers before opening the current one
+      questions.forEach((q) => {
+        const ans = q.querySelector(".answer");
+        const icon = q.querySelector("i");
+        ans.classList.remove("d-block");
+        ans.classList.add("d-none");
+        icon.classList.remove("rotate-135deg");
+      });
+
+      // Show the clicked answer
+      answer.classList.add("d-block");
+      answer.classList.remove("d-none");
+      plusIcon.classList.add("rotate-135deg");
+    }
+  }
+
+  question.addEventListener("click", showTheAnswer);
 });
 
 // == Event listener ==
 hamburgerMenu.addEventListener("click", toggleMenu);
-// window.addEventListener("scroll", showBackToTopButton);
-// backToTopButton.addEventListener("click", backToTop);
+window.addEventListener("scroll", showBackToTopButton);
+backToTopButton.addEventListener("click", backToTop);
